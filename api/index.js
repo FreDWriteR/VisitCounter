cf47dc592670338164674ec924eaabbf7a2188ea
Фёдор Дзиковский <fredwriter@yandex.ru>
2024-09-16 10:18:07 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Настройки базы данных
 const db = new sqlite3.Database(':memory:');
